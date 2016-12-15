879c58ae16fdebdc36a5e544fc65659c7a1235fa
Jason Stevens <jay@wizardofthenet.com>
2016-12-15 01:10:49 -0800

    add vs2015 project

---
+++ dgpulldown_windows_vs2015/Resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by DGPulldown.rc
+//
+#define IDD_MAIN                        101
+#define IDD_ICON                        102
+#define IDC_INPUT                       1001
+#define IDC_STREAM                      1002
+#define IDC_OUTPUT                      1002
+#define IDC_CONVERT                     1003
+#define IDC_STATUS                      1004
+#define IDC_RATE0                       1005
+#define IDC_RATE1                       1006
+#define IDC_RATE2                       1007
+#define IDC_RATE3                       1008
+#define IDC_RATE4                       1009
+#define IDC_BROWSE                      1010
+#define IDC_PROGRESS                    1011
+#define IDC_INPUT_RATE                  1012
+#define IDC_OUTPUT_RATE                 1013
+#define IDC_TIMECODES                   1014
+#define IDC_DROPFRAMES                  1015
+#define IDC_HH                          1016
+#define IDC_STARTTIME                   1017
+#define IDC_MM                          1018
+#define IDC_SS                          1019
+#define IDC_FF                          1020
+#define IDC_DEBUG                       1021
+#define IDC_NOOUTPUT                    1022
+#define IDC_NOOUTPUT2                   1023
+#define IDC_TFF                         1023
+#define IDC_INPLACE                     1024
+#define IDC_BROWSE_DEST                 1025
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
